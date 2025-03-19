d58dd19c5285f397e5059cba886243366255a62c
Rustam Rustamov <rustamov_rustam_ruslanovich@mail.ru>
2025-03-19 20:02:03 +0200

    Header: rework

--- scripts/Header.js
+++ scripts/Header.js
@@ -3,6 +3,7 @@ class Header {
     root: '[data-js-header]',
     overlay: '[data-js-header-overlay]',
     burgerButton: '[data-js-header-burger-button]',
+    menuLinks: '[data-js-header-links]', 
   }
 
   stateClasses = {
@@ -11,21 +12,41 @@ class Header {
   }
 
   constructor() {
-    this.rootElement = document.querySelector(this.selectors.root)
-    this.overlayElement = this.rootElement.querySelector(this.selectors.overlay)
-    this.burgerButtonElement = this.rootElement.querySelector(this.selectors.burgerButton)
-    this.bindEvents()
+    this.rootElement = document.querySelector(this.selectors.root);
+    this.overlayElement = this.rootElement.querySelector(this.selectors.overlay);
+    this.burgerButtonElement = this.rootElement.querySelector(this.selectors.burgerButton);
+    this.menuLinks = this.rootElement.querySelectorAll(this.selectors.menuLinks);
+
+    this.bindEvents();
   }
 
   onBurgerButtonClick = () => {
-    this.burgerButtonElement.classList.toggle(this.stateClasses.isActive)
-    this.overlayElement.classList.toggle(this.stateClasses.isActive)
-    document.documentElement.classList.toggle(this.stateClasses.isLock)
+    this.toggleMenu();
+  }
+
+  onLinkClick = () => {
+    this.closeMenu();
+  }
+
+  toggleMenu() {
+    this.burgerButtonElement.classList.toggle(this.stateClasses.isActive);
+    this.overlayElement.classList.toggle(this.stateClasses.isActive);
+    document.documentElement.classList.toggle(this.stateClasses.isLock);
+  }
+
+  closeMenu() {
+    this.burgerButtonElement.classList.remove(this.stateClasses.isActive);
+    this.overlayElement.classList.remove(this.stateClasses.isActive);
+    document.documentElement.classList.remove(this.stateClasses.isLock);
   }
 
   bindEvents() {
-    this.burgerButtonElement.addEventListener('click', this.onBurgerButtonClick)
+    this.burgerButtonElement.addEventListener('click', this.onBurgerButtonClick);
+
+    this.menuLinks.forEach(link => {
+      link.addEventListener('click', this.onLinkClick);
+    });
   }
 }
 
-export default Header
+export default Header;
