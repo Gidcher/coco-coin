a30df3a89fe03b760be0e5d1a2f3fe44388b6eaf
Rustam Rustamov <rustamov_rustam_ruslanovich@mail.ru>
2025-04-30 19:00:57 +0300

    Form remove

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,7 @@
 import Header from "./Header.js";
 import TokenPriceCollection from "./TokenPrice.js";
-import FeedbackFormCollection from "./FeedbackForm.js";
 
 new Header()
-new FeedbackFormCollection()
 document.addEventListener("DOMContentLoaded", () => {
   new TokenPriceCollection();
 });
